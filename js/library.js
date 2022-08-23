a2c9804005308bd7e5981033449cdf99d5da98d7
Ïgnacio <ifcortazzo@gmail.com>
2022-08-23 16:48:28 -0300

    first commit

---
+++ js/library.js
@@ -0,0 +1,10 @@
+
+//Style.display = "None" , oculta secciones, block muestra. Con "block" se pueden mostrar
+
+function  ocultarSeccionMain(){
+
+document.querySelector("#seccionMain").style.display = "none"
+
+
+
+}
